59be692bb0a36760ce519a14153e16ed5b9f8828
Corinne Winthrop <corinne.winthrop@simuwatt.com>
2019-03-18 11:29:51 -0600

    Add comments to understand better

--- server2.js
+++ server2.js
@@ -3,11 +3,22 @@ var express_graphql = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 // GraphQL schema
+// consists of a custom type Course and two query actions
+// defined query actions enable the user to
+// retrieve a single course by ID or
+// an array of courses by topic
+
+// the schema also contains a Mutation type
+// the Mutation type is named updateCourseTopic (contains two required params)
+// the return type is after the colon. The return type is of type Course
 var schema = buildSchema(`
     type Query {
         course(id: Int!): Course
         courses(topic: String): [Course]
     },
+    type Mutation {
+        updateCourseTopic(id: Int!, topic: String!): Course
+    }
     type Course {
         id: Int
         title: String
@@ -18,6 +29,7 @@ var schema = buildSchema(`
     }
 `);
 
+// dummy course data
 var coursesData = [
     {
         id: 1,
@@ -61,10 +73,50 @@ var getCourses = function(args) {
     }
 }
 
+// connecting the course query action to the getCourse function and
+// connecting the courses query action to the getCourses function
 var root = {
     course: getCourse,
     courses: getCourses
 };
+
+
+/////// Sample query code
+// the exclamation mark at the end of the parameter means it's required
+// you supply this parameter in the Query variables text area of the graphQL IDE
+
+// query getSingleCourse($courseID: Int!) {
+//   course(id: $courseID) {
+//       title
+//       author
+//       description
+//       topic
+//       url
+//   }
+// }
+
+
+// can include multiple queries in one request
+// course1 and course2 are aliases to distinguish between queries,
+// and will return like that in the response
+
+// query getCourseWithFragments($courseID1: Int!, $courseID2: Int!) {
+//   course1: course(id: $courseID1) {
+//          ...courseFields
+//   },
+//   course2: course(id: $courseID2) {
+//         ...courseFields
+//   } 
+// }
+
+// fragment courseFields on Course {
+//   title
+//   author
+//   description
+//   topic
+//   url
+// }
+
 
 // Create an express server and a GraphQL endpoint
 var app = express();
